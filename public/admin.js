63a0c5e2368a778dfff1e2642a4f4028f8f7bbc1
Alex_Mangushev <mangushev2001@yandex.ru>
2021-06-10 18:39:58 +0300

    V7

--- public/admin.js
+++ public/admin.js
@@ -1,5 +1,5 @@
 const root = document.getElementById('root')
-const domain = 'http://https://workeronline.herokuapp.com/admin.html'
+const domain = 'https://localhost'
 
 const login_Page = /* html */`
 <form class="login-form" onsubmit="loginSubmit(event)">
